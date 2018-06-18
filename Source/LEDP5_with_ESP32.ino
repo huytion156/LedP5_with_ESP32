b6f98f1785ad069bd8b44ab73fe0ae917678f8c9
Huynh Vo Nhat Huy <huytion156@gmail.com>
2018-06-18 13:17:37 +0700

    Update v2.0 with autoconnect wifi using hotspot

--- Source/LEDP5_with_ESP32.ino
+++ Source/LEDP5_with_ESP32.ino
@@ -3,14 +3,27 @@
 #include "freertos/task.h"
 #include "Adafruit_GFX.h"
 #include "ESP32RGBmatrixPanel.h"
-#include <WiFi.h>
 #include <HTTPClient.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+
+
+#include <DNSServer.h>
+#if defined(ESP8266)
+#include <ESP8266WebServer.h>
+#else
+#include <WebServer.h>
+#endif
+#include <WiFiManager.h>
 
 /*Khai bao Wifi*/
 const char* ssid = "CMA";
 const char* pass = "cma2018LHP515";
 
-/*So do chan cam LED P5*/
+
 //G1  R1 |
 //GND B1 |
 //G2  R2 |
@@ -37,15 +50,15 @@ const char* pass = "cma2018LHP515";
 
 ESP32RGBmatrixPanel matrix(23, 22, 03, 17, 16, 04, 13, 02, 15, 21, 19, 18, 5); //Flexible connection
 
-/*Danh sach 4 nhan vien tren LED*/
 struct dataName {
   String maSoThe;
   String kg;
-  bool redText; 
+  bool redText;
   dataName() {};
-  dataName(String ms, String kl, bool is_red) : maSoThe(ms), kg(kl), redText(is_red) {};  
+  dataName(String ms, String kl, bool is_red) : maSoThe(ms), kg(kl), redText(is_red) {};
 };
-dataName listName[4];
+
+dataName listName[4] = {dataName("012", "4.23", false), dataName("124", "4.11", false), dataName("192", "4.03", false), dataName("023", "3.83", true)};
 
 /* create a hardware timer */
 hw_timer_t* displayUpdateTimer = NULL;
@@ -54,47 +67,67 @@ void IRAM_ATTR onDisplayUpdate() {
 }
 
 void printName(dataName data, int pos) {
-  matrix.setCursor(2, pos * 8);
-  
   if (data.redText)
     matrix.setTextColor(matrix.AdafruitColor(255, 0, 0));
   else matrix.setTextColor(matrix.AdafruitColor(0, 255, 0));
-  
+
+  matrix.setCursor(2, pos * 8);
   matrix.print(data.maSoThe);
-  matrix.print(" : ");
+  matrix.print(": ");
   matrix.print(data.kg);
   vTaskDelay(10);
 }
 
+
+void splash_screen(int c) {
+  Serial.println("Slash");
+  matrix.black();
+  matrix.setTextColor(matrix.AdafruitColor(0, 255, 0));
+  matrix.setCursor(20, 2);
+  matrix.print("WIFI");
+  matrix.setCursor(2, 14);
+  matrix.print("CONNECTING");
+  matrix.setCursor(1, 20);
+  char s[20];
+  for (int i = 0; i < c; i++) {
+    s[i] = '.';
+  }
+  s[c] = 0;
+  matrix.print(s);
+}
+
 volatile bool is_update = false;
+volatile bool is_wifi_connecting = false;
+volatile int count_c = 0;
 
 //runs faster then default void loop(). why? runs on other core?
 void loop2_task(void *pvParameter)
 {
   while (true) {
-    vTaskDelay(1000);
-    if (is_update) {
-      is_update = false;
+    if (is_wifi_connecting) {
+      
+      splash_screen(count_c);
+      vTaskDelay(500);
+      count_c += 1;
+      if (count_c>8) count_c = 1;
+    } else {
+      vTaskDelay(1000);
+      if (is_update) {
+        is_update = false;
+
+      }
     }
   }
 }
 
 long prev_t;
+long prev_timeStamp;
+
 void setup() {
   Serial.begin(115200);
-
-  /*Ket noi WiFi*/
-  delay(4000);
-  WiFi.begin(ssid, pass);
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to the WiFi network");
-  
-  matrix.setBrightness(1); //Dieu chinh do sang cua LED 
+  //matrix.setBrightness(255);
   is_update = true;
-
+  is_wifi_connecting = true;
   xTaskCreate(&loop2_task, "loop2_task", 2048, NULL, 5, NULL);
   /* 1 tick take 1/(80MHZ/80) = 1us so we set divider 80 and count up */
   displayUpdateTimer = timerBegin(0, 120, true);
@@ -103,43 +136,65 @@ void setup() {
   timerAttachInterrupt(displayUpdateTimer, &onDisplayUpdate, true);
   timerAlarmWrite(displayUpdateTimer, 2, true);
   timerAlarmEnable(displayUpdateTimer);
-  
+
+
+  prev_timeStamp = -1;
+  /*Ket noi WiFi*/
+  //matrix.black();
+  //splash_screen();
+  delay(5000);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Bang LED 01");
+  is_wifi_connecting = false;
+
+
+  Serial.println("OK");
   prev_t = millis();
-  
+
 }
 
 
 void loop() {
-  //Kiem tra ket noi internet 
   if ((WiFi.status() == WL_CONNECTED) && (millis() - prev_t > 1000)) {
     HTTPClient http;
-    http.begin("http://192.168.1.55:3000/person"); //Server tra JSON 
+    http.begin("http://192.168.1.56/get_last_info.php");
     int httpCode = http.GET();
-    
-    if (httpCode > 0) {
-      String payload= http.getString();
-      //Serial.println(httpCode);
-      //Serial.println(payload);
 
-      /*Nhan chuoi JSON va Parse*/
-      StaticJsonBuffer<500> JSONBuffer;
+    if (httpCode > 0) {
+      String payload = http.getString();
+      //Serial.println(httpCode);
+      Serial.println(payload);
+      //Serial.println(payload);
+      StaticJsonBuffer<1000> JSONBuffer;
       JsonObject& parsed = JSONBuffer.parseObject(payload);
 
-      const char* MST = parsed["MST"][0];
-      const char* KL  = parsed["KL"][0];
-      const bool isRed= parsed["isRed"][0];
+      //const char* MST = parsed["MST"][0];
+      //const char* KL  = parsed["KL"][0];
+      //const bool isRed = parsed["isRed"][0];
+      const char* ts = parsed["ts"];
+      //Serial.println(ts);
+      long timeStamp = atol(ts);
+      Serial.println(timeStamp);
+      if (prev_timeStamp < timeStamp) {
 
-      listName[0] = dataName(parsed["MST"][0], parsed["KL"][0], parsed["isRed"][0]);
-      listName[1] = dataName(parsed["MST"][1], parsed["KL"][1], parsed["isRed"][1]);
-      listName[2] = dataName(parsed["MST"][2], parsed["KL"][2], parsed["isRed"][2]);
-      listName[3] = dataName(parsed["MST"][3], parsed["KL"][3], parsed["isRed"][3]);
+        prev_timeStamp = timeStamp;
+        listName[0] = dataName(parsed["MST"][0], parsed["KL"][0], parsed["isRed"][0]);
+        listName[1] = dataName(parsed["MST"][1], parsed["KL"][1], parsed["isRed"][1]);
+        listName[2] = dataName(parsed["MST"][2], parsed["KL"][2], parsed["isRed"][2]);
+        listName[3] = dataName(parsed["MST"][3], parsed["KL"][3], parsed["isRed"][3]);
 
-      /*In danh sach ra bang LED*/
-      for(int i = 0; i < 4; i++) printName(listName[i], i);      
+        matrix.black();
+
+        for (int i = 0; i < 4; i++) {
+          printName(listName[i], i);
+        }
+
+      }
     }
-    else Serial.println("Error on HTTP request"); //Request that bai 
+    else Serial.println("Error on HTTP request");
     http.end();
     is_update = true;
     prev_t = millis();
+
   }
 }
